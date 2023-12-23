4a63efb18541214622fc8cd6a82b183063cd4cde
Leeren Chang <leeren@leerenchang.com>
2023-12-23 13:05:28 -0800

    Update dependency to use v1 router

--- test/TurtleWifPants.Liquidity.t.sol
+++ test/TurtleWifPants.Liquidity.t.sol
@@ -3,18 +3,16 @@ pragma solidity ^0.8.10;
 
 import "forge-std/console2.sol";
 
-import { TestHelper } from "joe-v2-test/helpers/TestHelper.sol";
-import { AvalancheAddresses } from "joe-v2-test/integration/Addresses.sol";
+import { TestHelper } from "joe-v2-test/TestHelper.sol";
 
 /// @notice Tests for LP provisioning using the Joe V1 Router.
 contract TurtleWifPantsLiquidityTest is TestHelper {
 
-    function setUp() public override {
-        super.setUp();
+    function setUp() public {
     }
     
     function test_FactoryAddress() public {
-        assertEq(address(routerV1), AvalancheAddresses.JOE_V1_ROUTER, "test_FactoryAddress");
+        assertEq(address(routerV1), JOE_V1_ROUTER_ADDRESS, "test_FactoryAddress");
     }
 
 
